b839b5898bcaf34429a696ab820335c629254be1
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2022-09-29 12:38:59 -0400

    added bottom content h4 and paragraph

--- src/index.js
+++ src/index.js
@@ -72,3 +72,13 @@ topContent.children[0].children[1].textContent = siteContent["main-content"]["fe
 topContent.children[1].children[0].textContent = siteContent["main-content"]["about-h4"];
 topContent.children[1].children[1].textContent = siteContent["main-content"]["about-content"];
 
+// Bottom Content
+const bottomContent = document.querySelector('.bottom-content');
+const h4sBottom = bottomContent.querySelectorAll("h4");
+h4sBottom[0].textContent = siteContent["main-content"]["services-h4"];
+h4sBottom[1].textContent = siteContent["main-content"]["product-h4"];
+h4sBottom[2].textContent = siteContent["main-content"]["vision-h4"];
+const paraBottom = bottomContent.querySelectorAll('p');
+paraBottom[0].textContent = siteContent["main-content"]["services-content"];
+paraBottom[1].textContent = siteContent["main-content"]["product-content"];
+paraBottom[2].textContent = siteContent["main-content"]["vision-content"];
